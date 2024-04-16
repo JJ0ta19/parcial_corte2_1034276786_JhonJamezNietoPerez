577604e5b0ee15ec4853f99c2621c4baa092a6ce
JJ0ta19 <jjnieto-2022a@corhuila.edu.co>
2024-04-16 06:46:29 -0500

    Cambios finales

---
+++ frontend/restaurante/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  
+  {
+    path: '',
+    redirectTo: 'pedido',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pedido',
+    loadChildren: () => import('./pedido/pedido.module').then( m => m.PedidoPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
